549173f71fde2f5542ad64de8a72b02171b0f332
mhalley <marguerite.e.halley@gmail.com>
2024-08-05 18:04:03 -0400

    Add server.py to deploy Flask server and add function to pass form input text to emotion_detector function  and display response in interface.

---
+++ server.py
@@ -0,0 +1,44 @@
+''' Executing this function initiates the application of emotion
+    detector to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask framework package :
+from flask import Flask, render_template, request
+
+# Import the emotion_detector function from the package created:
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate the flask app :
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detect():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion analysis over it using emotion_detector()
+        function. The output returned shows the scores for all emotions and the dominant emotion.
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the emotion detector function and store the response
+    response = emotion_detector(text_to_analyze)
+    # Extract the emotions and dominant emotion from the response
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    # Return a formatted string with the emotions and dominant emotion
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. The dominant emotion is <strong>{dominant_emotion}</strong>."
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
