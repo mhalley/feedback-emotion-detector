1544399b7c29143dc1fcdf5d03f83eeab2ecf7e3
mhalley <marguerite.e.halley@gmail.com>
2024-08-05 16:05:37 -0400

    Format response to extract scores for specific emotions

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,20 @@
-
+import json
 import requests
 
 def emotion_detector(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyze)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion detector service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    response = requests.post(url, json = myobj, headers=header, timeout=10)  # Send a POST request to the API with the text and headers
+
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+    # Extracting emotions from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    # Returning a dictionary containing sentiment analysis results
+    return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score}
+
